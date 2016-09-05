c63c1639943f8aca5ebc2986154751c391576420
denar90 <denysov.artem@gmail.com>
2016-09-05 23:49:25 +0300

    Backbone extend instead of classify

--- src/backbone.service.js
+++ src/backbone.service.js
@@ -1,11 +1,39 @@
-import classify from 'backbone-metal-classify';
 import Radio from 'backbone.radio';
 import _ from 'underscore';
 import PromisePolyfill from 'es6-promise';
 
 const resolved = PromisePolyfill.Promise.resolve();
 
-Radio.Channel = classify(Radio.Channel);
+//copy of Backbone.js extend function
+Radio.Channel.extend = function(protoProps, staticProps) {
+  const parent = this;
+  let child;
+
+  // The constructor function for the new subclass is either defined by you
+  // (the "constructor" property in your `extend` definition), or defaulted
+  // by us to simply call the parent constructor.
+  if (protoProps && _.has(protoProps, 'constructor')) {
+    child = protoProps.constructor;
+  } else {
+    child = function() {
+      return parent.apply(this, arguments);
+    };
+  }
+
+  // Add static properties to the constructor function, if supplied.
+  _.extend(child, parent, staticProps);
+
+  // Set the prototype chain to inherit from `parent`, without calling
+  // `parent`'s constructor function and add the prototype properties.
+  child.prototype = _.create(parent.prototype, protoProps);
+  child.prototype.constructor = child;
+
+  // Set a convenience property in case the parent's prototype is needed
+  // later.
+  child.__super__ = parent.prototype;
+
+  return child;
+};
 
 /**
  * @class Service
@@ -29,7 +57,7 @@ export default Radio.Channel.extend({
       });
     });
 
-    this._super(...arguments);
+    Radio.Channel.prototype.constructor.apply(this, ...arguments);
   },
 
   /**
